636173d3ec9e7539077a3e4c0a4891bf768671c5
varun0310t <chauhanvarun10th03@gmail.com>
2025-07-27 14:55:40 +0530

    paused linting

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ export const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [{ protocol: "https", hostname: "img.clerk.com" }],
   },
