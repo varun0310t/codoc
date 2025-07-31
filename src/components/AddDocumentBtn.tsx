f8a8283b38c185510d03ea776e1484f8d23bf91b
varun0310t <chauhanvarun10th03@gmail.com>
2025-07-31 17:21:03 +0530

    fixed 404 when creating new documents

--- src/components/AddDocumentBtn.tsx
+++ src/components/AddDocumentBtn.tsx
@@ -15,7 +15,7 @@ const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
     try {
       const room = await createDocument({ userId, email });
 
-      if (room) router.push(`/documents/${room.id}`);
+      if (room) router.push(`/dashboard/documents/${room.id}`);
     } catch (error) {
       console.log(error);
     }
